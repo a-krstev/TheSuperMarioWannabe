cd6eb9790a2f242b57d87dc94d6b93bb2dc54bfb
Atanas Krstev <example@site.com>
2019-06-02 20:02:23 +0200

    Add initial background

--- The Super Mario Wannabe/The Super Mario Wannabe/Form1.cs
+++ The Super Mario Wannabe/The Super Mario Wannabe/Form1.cs
@@ -12,9 +12,17 @@ namespace The_Super_Mario_Wannabe
 {
     public partial class Form1 : Form
     {
+        public Level1 testLevel { get; set; }
         public Form1()
         {
             InitializeComponent();
+            testLevel = new Level1();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //e.Graphics.Clear(Color.White);
+            testLevel.Draw(e.Graphics);
         }
     }
 }
---
+++ The Super Mario Wannabe/The Super Mario Wannabe/Level1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Super_Mario_Wannabe
+{
+    public class Level1 : ILevel
+    {
+        private readonly Image blockImage = Properties.Resources.GenericBlock;
+        public int MyProperty { get; set; }
+
+        public Level1()
+        {
+
+        }
+
+        public void Draw(Graphics g)
+        {
+            for (int i = 0; i < 19; i++)
+            {
+                for (int j = 0; j < 25; j++)
+                {
+                    if ( j == 0 || j == 24)
+                    {
+                        g.DrawImageUnscaled(blockImage, new Point(j * blockImage.Width, i * blockImage.Height));
+                    }
+
+                    if ( (i == 0 || i == 7 || i == 15) && (j < 3 || j > 5) )
+                    {
+                        g.DrawImageUnscaled(blockImage, new Point(j * blockImage.Width, i * blockImage.Height));
+                    }
+
+                    if ( (i == 3 || i == 11) && (j < 19 || j > 21))
+                    {
+                        g.DrawImageUnscaled(blockImage, new Point(j * blockImage.Width, i * blockImage.Height));
+                    }
+                }
+            }
+        }
+    }
+}
